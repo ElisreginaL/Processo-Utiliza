8d398a935ad6e01b158e1d68e918956b02a0e7b6
Allison Ribeiro <dev.allisonribeiro@gmail.com>
2024-11-23 15:58:09 -0300

    router fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import Home from './pages/Home/Home';
 import Assistencia24h from './pages/Setores/Assistencia24h/Assistencia24h'; // Página inicial do setor Assistência 24h
 import Comunicado from './pages/Setores/Comunicado/Comunicado'; // Página do setor Comunicado
 import Recebiveis from './pages/Setores/Recebiveis/Recebiveis'; // Página do setor Recebíveis
-import RotasSolidy from './pages/Setores/Assistencia24h/Solidy/rotas'; // Arquivo de rotas da Solidy
+// import RotasSolidy from './pages/Setores/Assistencia24h/Solidy/rotas'; // Arquivo de rotas da Solidy
+import Solidy from './pages/Setores/Assistencia24h/Solidy/Solidy';
+import Acionamento from './pages/Setores/Assistencia24h/Solidy/Acionamento/Acionamento';
 
 import Footer from './components/Footer/Footer'; // Importando o rodapé
 import './App.scss';  // Importando os estilos globais
@@ -19,14 +21,17 @@ function App() {
           <Route path="/" element={<Home />} />
 
           {/* Rota para o setor Assistência 24h */}
-          <Route path="/assistencia24h/*" element={<Assistencia24h />} />
-          <Route path="/assistencia24h/solidy/*" element={<RotasSolidy />} />
+          <Route path="assistencia24h" element={<Assistencia24h />} />
+          <Route path="assistencia24h/solidy" element={<Solidy />} />
+          <Route path="assistencia24h/solidy/acionamento" element={<Acionamento />} />
+         
+          
 
           {/* Rota para o setor Comunicado */}
-          <Route path="/comunicado/*" element={<Comunicado />} />
+          <Route path="comunicado/*" element={<Comunicado />} />
 
           {/* Rota para o setor Recebíveis */}
-          <Route path="/recebiveis/*" element={<Recebiveis />} />
+          <Route path="recebiveis/*" element={<Recebiveis />} />
         </Routes>
       </div>
     </div>
